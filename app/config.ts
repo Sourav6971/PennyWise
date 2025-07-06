89512e4e0c5492336fc162304012b7d7c2c61fef
sourav <mailsourav014@gmail.com>
2025-07-06 16:47:39 +0530

    added backend url

--- app/config.ts
+++ app/config.ts
@@ -1,2 +1,2 @@
-// export const BACKEND_URL = "https://penny-wise-phi.vercel.app";
-export const BACKEND_URL = "http://localhost:3001";
+export const BACKEND_URL = "https://penny-wise-phi.vercel.app";
+
